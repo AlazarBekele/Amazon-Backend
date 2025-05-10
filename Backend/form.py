cb9787d318bb802fe8b922ac6d4a38475aa9d523
Alazar <alazarthe70@gmail.com>
2025-05-10 18:09:10 +0300

    Successfuly add user creation

--- Backend/form.py
+++ Backend/form.py
@@ -9,7 +9,7 @@ class Login_Input (UserCreationForm):
 
     'class' : 'form-control',
     'placeholder' : 'First Name',
-    'autocomplate' : 'off'
+    'autocomplete' : 'off'
 
   }))
 
@@ -17,42 +17,41 @@ class Login_Input (UserCreationForm):
 
     'class' : 'form-control',
     'placeholder' : 'Last Name',
-    'autocomplate' : 'off'
+    'autocomplete' : 'off'
 
   }))
 
-  username = forms.CharField (max_length=20, widget = forms.TextInput(attrs={
+  username = forms.CharField (widget = forms.TextInput(attrs={
 
     'class' : 'form-control',
     'placeholder' : 'User Name',
-    'autocomplate' : 'off'
+    'autocomplete' : 'off'
 
   }))
 
-  Email = forms.EmailField (max_length=20, widget = forms.EmailInput(attrs={
+  Email = forms.EmailField (widget = forms.EmailInput(attrs={
 
     'class' : 'form-control',
     'placeholder' : 'Enter Your Email',
-    'autocomplate' : 'off'
+    'autocomplete' : 'off'
 
   }))
 
-  password1 = forms.PasswordInput (max_length=20, widget = forms.PasswordInput(attrs={
-
-    'class' : 'form-control',
-    'placeholder' : 'Password',
-    'autocomplate' : 'off'
-
+  password1 = forms.CharField (max_length=40, widget = forms.PasswordInput(attrs={
+     
+     'class' : 'form-control parkinsans',
+     'placeholder' : 'Enter Password1',
+     'autocomplete': 'off'
   }))
+      
+  password2 = forms.CharField (max_length=40, widget = forms.PasswordInput(attrs={
 
-  password2 = forms.PasswordInput (max_length=20, lable='', widget = forms.PasswordInput(attrs={
-
-    'class' : 'form-control',
-    'placeholder' : 'Password confirm',
-    'autocomplate' : 'off'
+    'class' : 'form-control parkinsans',
+    'placeholder' : 'Enter Password1',
+    'autocomplete': 'off'
 
   }))
 
   class Meta:
     model = User
-    fields = ('First_name', 'Last_name', 'username', 'Email', 'password1', 'password2')
+    fields = ['First_name', 'Last_name', 'username', 'Email', 'password1', 'password2']
