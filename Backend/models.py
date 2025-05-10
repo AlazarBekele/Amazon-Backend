994ce9592529749c49dbdf14d4c23d983310b7c5
Alazar <alazarthe70@gmail.com>
2025-05-10 13:05:23 +0300

    Create New Model to store data base

--- Backend/models.py
+++ Backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Login_IMG (models.Model):
+
+  title = models.CharField (max_length=20)
+  picture = models.ImageField (upload_to='photo/')
+
+  def __str__(self):
+    return self.title
