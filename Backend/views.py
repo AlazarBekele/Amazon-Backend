58ec0812e945b2278b78e89ec23da51de14174c7
Alazar <alazarthe70@gmail.com>
2025-05-10 17:50:02 +0300

    Finsh the view, Post, Add redirect

--- Backend/views.py
+++ Backend/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Login_IMG
+from .form import Login_Input
+
 
 # Create your views here.
 
@@ -20,6 +22,14 @@ def sell_index (request):
 def login_here (request):
 
   img_container = Login_IMG.objects.all()
+  Login_form = Login_Input (request.POST or None)
+
+
+  if request.method == 'POST':
+    if Login_Input.is_valid():
+
+      Login_Input.save()
+      return redirect ('index')
 
   context = {
     'IMG_cont' : img_container
